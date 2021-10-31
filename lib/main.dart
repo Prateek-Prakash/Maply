3c8ee73d53e7143f08f12fba8f661b257e356bba
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2021-10-31 15:31:03 -0400

    Add Basic FloatingSearchBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:get_it/get_it.dart';
 import 'package:get_it_hooks/get_it_hooks.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 final getIt = GetIt.instance;
 void setupGetIt() {
@@ -50,36 +51,13 @@ class AppShellView extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Maply'),
-        centerTitle: true,
-      ),
-      body: FutureBuilder(
-        future: _getCurrentLocation(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            Position currPosition = snapshot.data as Position;
-            CameraPosition camPosition = CameraPosition(
-              target: LatLng(currPosition.latitude, currPosition.longitude),
-              zoom: 15.0,
-            );
-            return GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: camPosition,
-              onMapCreated: (GoogleMapController controller) {
-                _mapController.complete(controller);
-              },
-              zoomControlsEnabled: false,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              tiltGesturesEnabled: false,
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          _buildMap(),
+          _buildFloatingSearchBar(),
+        ],
       ),
     );
   }
@@ -87,6 +65,73 @@ class AppShellView extends HookWidget {
   Future<Position> _getCurrentLocation() async {
     return await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.bestForNavigation,
+    );
+  }
+
+  Widget _buildMap() {
+    return FutureBuilder(
+      future: _getCurrentLocation(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          Position currPosition = snapshot.data as Position;
+          CameraPosition camPosition = CameraPosition(
+            target: LatLng(currPosition.latitude, currPosition.longitude),
+            zoom: 15.0,
+          );
+          return GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: camPosition,
+            onMapCreated: (GoogleMapController controller) {
+              _mapController.complete(controller);
+            },
+            zoomControlsEnabled: false,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false,
+            tiltGesturesEnabled: false,
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildFloatingSearchBar() {
+    return FloatingSearchBar(
+      hint: 'Search Ongoing Pins',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: 0.0,
+      openAxisAlignment: 0.0,
+      width: 600,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {},
+      transition: CircularFloatingSearchBarTransition(),
+      leadingActions: [
+        FloatingSearchBarAction.hamburgerToBack(),
+      ],
+      actions: [
+        FloatingSearchBarAction.searchToClear(),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
